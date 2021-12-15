68145630d8fe84ef94699ff69f70c54dc2e4fd90
jinyunjae <jinyun3075@naver.com>
2021-12-15 19:19:50 +0900

    option

--- js/script.js
+++ js/script.js
@@ -3,4 +3,21 @@ function a(){
     let lang = document.querySelector('.list-lang');
     lang.classList.toggle('on');    
 }
-btn.addEventListener('click', a);
+btn.onclick = a;
+
+let header = document.querySelector('.header');
+let logo = document.querySelector('.logo');
+let list =document.querySelector('.list-lang');
+window.onscroll =function() {
+    let tran = [header,logo,list];
+    if(window.scrollY==0) {
+        tran.forEach(value=>{
+            value.classList.remove('op');
+        })
+    } else {
+        tran.forEach(value=>{
+            value.classList.add('op');
+        })
+    }
+};
+
