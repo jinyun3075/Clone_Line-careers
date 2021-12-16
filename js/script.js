20fe56671975aa212bbf1468c09a0766e3789cdc
jinyunjae <jinyun3075@naver.com>
2021-12-16 19:55:01 +0900

    완성본

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ const btn = document.querySelector('.btn-lang');
 const btncombo = document.querySelectorAll('.btn-combo');
 const btnSite = document.querySelector('.btn-site');
 const snslist =document.querySelectorAll('.snslist');
+const btnScrollTop = document.querySelector(".btn-scroll-top");
+const header = document.querySelector('.header');
+const logo = document.querySelector('.logo');
+const list =document.querySelector('.list-lang');
+const chg = document.querySelectorAll('.cont-combo ul li');
+
+
 function logic (){
     const end =this.parentNode.parentNode.querySelectorAll('ul');
     const list = this.parentNode.querySelector('ul');
@@ -35,14 +42,6 @@ function logic2(){
     list.classList.toggle('on');
 }
 
-btn.onclick = logic;
-btncombo.forEach((e)=>e.onclick=logic);
-btnSite.onclick =logic2;
-snslist.forEach((e)=>e.onclick=logic2);
-
-let header = document.querySelector('.header');
-let logo = document.querySelector('.logo');
-let list =document.querySelector('.list-lang');
 window.onscroll =function() {
     let tran = [header,logo,list];
     if(window.scrollY==0) {
@@ -56,8 +55,6 @@ window.onscroll =function() {
     }
 };
 
-const chg = document.querySelectorAll('.cont-combo ul li');
-chg.forEach((e)=>e.onclick=change);
 function change() {
     let changeBtn = 'a';
     let word = this.querySelector('a').innerText;
@@ -66,3 +63,26 @@ function change() {
     ch.parentNode.querySelector('ul').classList.toggle('on');
     
 }
+
+function scrollUpEvent() {
+        window.onmousewheel = function (e) {
+        // 마우스 휠 페이지 상단 방향으로 이동할 때
+        if (e.wheelDelta === 120) {
+        // btnScrollTop 아래에서 위로 나타남
+        btnScrollTop.classList.add("scroll-up");
+        //
+        if (window.scrollY < 120) {
+            btnScrollTop.classList.remove("scroll-up");
+        }
+        } else {
+        btnScrollTop.classList.remove("scroll-up");
+        }
+    };
+}
+
+btn.onclick = logic;
+btncombo.forEach((e)=>e.onclick=logic);
+btnSite.onclick =logic2;
+snslist.forEach((e)=>e.onclick=logic2);
+chg.forEach((e)=>e.onclick=change);
+scrollUpEvent();
